51585e40afb9d18efecafc3671676939ce0dc49a
JulienDauphin <juliendauphin.openclassrooms@gmail.com>
2023-02-25 15:38:11 +0100

    Finalisation de la page commande + modification CSS

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -10,20 +10,18 @@ function theme_enqueue_styles()
 
 function modifier_menu_admin( $items, $args ) {
     if(is_super_admin() && $args->theme_location == 'main_menu'){
-    $items .= '<li id="menu-rencontrer" class="menu-item menu-item-type-post_type menu-item-object-page menu-rencontrer">
-    <a href="http://localhost/Planty/nous-rencontrer/" class="menu-link"><span class="text-wrap">Nous rencontrer</span></a></li>
-    <li id="menu-admin" class="nmr-administrator menu-item menu-item-type-custom menu-item-object-custom menu-item-407">
+    $items .= 
+    '<li id="menu-admin" class="">
     <a href="http://localhost/Planty/wp-admin/" class="menu-link"><span class="text-wrap">Admin</span></a></li>
-    <li id="menu-commander" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-69">
-    <a href="http://localhost/Planty/commander/" class="menu-link"><span class="text-wrap">Commander</span></a></li>';
+    <button id="menu-commander" class="">
+    <a href="http://localhost/Planty/commander/">Commander</a></button>';
     }
     else {
-        $items .= '<li id="menu-rencontrer" class="menu-item menu-item-type-post_type menu-item-object-page menu-rencontrer">
-        <a href="http://localhost/Planty/nous-rencontrer/" class="menu-link"><span class="text-wrap">Nous rencontrer</span></a></li>
-        <li id="menu-commander" class="menu-item menu-item-type-post_type menu-item-object-page menu-commander">
-        <a href="http://localhost/Planty/commander/" class="menu-link"><span class="text-wrap">Commander</span></a></li>';  
+        $items .= 
+        '<button id="menu-commander" class="">
+        <a href="http://localhost/Planty/commander/">Commander</a></button>';  
     }
     return $items;
 }
-add_filter('wp_nav_menu_items', 'modifier_menu_admin',1,2);
+add_filter('wp_nav_menu_items', 'modifier_menu_admin',10,2);
 ?>
